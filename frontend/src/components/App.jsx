809912210cbac17b4aebc16d416b5a2fd8d2b351
ale_wsl <alejo.ubeto@gmail.com>
2023-04-12 15:57:12 -0600

    [ROUTER] barrel files reference added to index

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import { LandingPage, IdePage } from '../routes/indexRoutes';
+import { LandingPage, IdePage, SignIn, SignUp } from '../routes/indexRoutes';
 
 function App() {
   return (
     <Routes>
       <Route path='/landing' element={<LandingPage />}/>
       <Route path='/compiler' element={<IdePage />}/>
+      <Route path='/signin' element={<SignIn />}/>
+      <Route path='/signup' element={<SignUp />}/>
     </Routes>
   );
 }
--- frontend/src/routes/indexRoutes.js
+++ frontend/src/routes/indexRoutes.js
@@ -1,2 +1,4 @@
 export { IdePage } from './IdePage';
 export { LandingPage } from './LandingPage';
+export { SignIn } from './SignIn';
+export { SignUp } from './SignUp';
