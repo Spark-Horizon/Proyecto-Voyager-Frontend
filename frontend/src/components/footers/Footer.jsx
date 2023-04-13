6924186b24dcf22d328aea87643a7ab0d1a6976f
Alastor963 <alaned01@gmail.com>
2023-04-13 01:20:52 -0600

    [COMPONENT] Footer creation (needs to be fixed)

---
+++ frontend/src/components/footers/Footer.jsx
@@ -0,0 +1,22 @@
+import { CustomButton } from '../buttons/indexButtons'
+
+export const Footer = () => {
+  const handleButtonClick = () => {
+    alert('SÍ SIRVE AAAAAA!');
+  }
+
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; SparkHorizon</p>
+          </div>
+          <div className="col-md-6">
+            <CustomButton text="Contáctanos" func={handleButtonClick} />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { Navbar } from "../components/navbars/indexNavbars"
+import { Footer } from "../components/footers/indexFooters"
 
 import '../styles/landingPage.css'
 
@@ -6,6 +7,7 @@ export const LandingPage = () => {
     return (
         <section className="landing-page">
             <Navbar />
+            <Footer />
         </section>
     )
 }
