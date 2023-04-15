17ac79467c01db2b05491096b8d9913fe8e4f5c1
Alastor963 <alaned01@gmail.com>
2023-04-15 01:32:27 -0600

    [COMPONENTS] Sign In page changes and Sign Up page implementation

--- frontend/src/routes/SignIn.jsx
+++ frontend/src/routes/SignIn.jsx
@@ -22,6 +22,10 @@ export const SignIn = () => {
         <div className="container d-flex justify-content-center align-items-center vh-100">
           <div className="card">
             <div className="card-body p-5">
+              <div className="text-center mb-4">
+                <h1 className="h3 mb-0">Iniciar sesión</h1>
+                <p className="lead">Ingresa tus datos para acceder</p>
+              </div>
               <form>
                 <div className="form-group">
                   <label htmlFor="email" className="text-center fs-4">Email</label>
@@ -30,16 +34,19 @@ export const SignIn = () => {
                 <div className="form-group mt-4">
                   <label htmlFor="password" className="text-center fs-4">Contraseña</label>
                   <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Contraseña" required />
+                  <div className="text-start mt-2">
+                    <a href="#" className="text-primary text-decoration-none fs-7">Olvidé mi contraseña</a>
+                  </div>
                 </div>
                 <button type="submit" className="btn btn-primary btn-block mt-4 d-flex justify-content-center align-items-center" onClick={handleSignIn} style={{ width: '100%' }}>
                   <span className="fs-5">Iniciar sesión</span>
                 </button>
               </form>
               <div className="text-center mt-4">
-                <span className="fs-5">¿No estás registrado?</span>
+                <span className="fs-5">¿No tienes una cuenta?</span>
               </div>
               <button type="button" className="btn btn-outline-secondary mx-auto mt-3 w-100" onClick={handleSignUp}>
-                <span className="fs-5">Crear cuenta</span>
+                <span className="fs-5">Registrarse</span>
               </button>
             </div>
           </div>
--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -1,5 +1,67 @@
+import { useState } from 'react';
+import { NavbarBasic } from '../components/navbars/NavbarBasic';
+
 export const SignUp = () => {
+  const [name, setName] = useState('');
+  const [lastName1, setLastName1] = useState('');
+  const [lastName2, setLastName2] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [terms, setTerms] = useState(false);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    alert('FUNCIONA');
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div>
+      <NavbarBasic />
+      <section className="sign-up">
+        <div className="container d-flex justify-content-center align-items-center vh-90">
+          <div className="card">
+            <div className="card-body p-5">
+                <div className="text-center mb-4">
+                  <h1 className="h3 mb-0">Registro de estudiante</h1>
+                  <p className="lead">Por favor ingresa la siguiente información</p>
+                </div>
+              <form>
+                <div className="form-group mb-4">
+                  <label htmlFor="name" className="text-center fs-4">Nombre completo</label>
+                  <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} className="form-control" placeholder="Nombre completo" required />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="last-name-1" className="text-center fs-4">Primer apellido</label>
+                  <input type="text" id="last-name-1" value={lastName1} onChange={(e) => setLastName1(e.target.value)} className="form-control" placeholder="Apellido paterno" required />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="last-name-2" className="text-center fs-4">Segundo apellido</label>
+                  <input type="text" id="last-name-2" value={lastName2} onChange={(e) => setLastName2(e.target.value)} className="form-control" placeholder="Apellido materno" required />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="password" className="text-center fs-4">Contraseña</label>
+                  <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Contraseña" required />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="confirm-password" className="text-center fs-4">Confirmar contraseña</label>
+                  <input type="password" id="confirm-password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="form-control" placeholder="Confirmar contraseña" required />
+                </div>
+                <div className="form-group mb-4">
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value={terms} onChange={(e) => setTerms(e.target.checked)} id="terms" />
+                    <label className="form-check-label" htmlFor="terms">
+                      Acepto los términos y condiciones
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block d-flex justify-content-center align-items-center" onClick={handleSignUp} disabled={!terms} style={{ width: '100%' }}>
+                  <span className="fs-5">Registrarse</span>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+     </section>    
+    </div>     
+  );
+};
