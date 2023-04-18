2f006c6f54aa1163b5233c5001e016a3f2349272
ale_wsl <alejo.ubeto@gmail.com>
2023-04-18 10:53:45 -0600

    [STYLES] minor changes

--- frontend/src/components/navbars/NavbarBasic.jsx
+++ frontend/src/components/navbars/NavbarBasic.jsx
@@ -1,16 +1,13 @@
 // Navbar responsiva intermedia entre landing e interfaz de usuario
 
-import logo from '../../assets/svg/isotipo_color.svg';
+// import logo from '../../assets/svg/isotipo_color.svg';
 
 export const NavbarBasic = () => {
 
   return (
 <nav className="navbar" id='navbar-basic'>
     <div className="container-fluid navbarContent">
-        <a className="navbar-brand container-c" href="/landing">
-          <img src={logo} alt="Logo" width="40" height="40" className="d-inline-block align-text-top me-2" />
-          iCode
-        </a>
+        <a className="navbar-brand container-c" href="/landing">iCode</a>
     </div>
 </nav>
   );
