45644d75512edb8517e1349693df457c271d817c
ale_wsl <alejo.ubeto@gmail.com>
2023-04-18 02:52:30 -0600

    [STYLES] Landing page V1

--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -7,6 +7,8 @@ export const LandingPage = () => {
     return (
         <section className="landing-page">
             <Navbar />
+            <div className="content">
+            </div>
             <Footer />
         </section>
     )
