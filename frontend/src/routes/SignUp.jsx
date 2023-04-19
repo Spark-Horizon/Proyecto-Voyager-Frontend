25e9df6ccc07619aa9312dd99f59dd895bc2ba63
ale_wsl <alejo.ubeto@gmail.com>
2023-04-19 01:44:42 -0600

    [ROUTE] new sign up route styles

--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NavbarBasic } from '../components/navbars/NavbarBasic';
+import { CustomButton } from '../components/buttons/indexButtons';
 
 export const SignUp = () => {
   const [step, setStep] = useState(1);
@@ -50,155 +51,175 @@ export const SignUp = () => {
   return (
     <div>
       <NavbarBasic />
-      <section className="sign-up">
-        <div className="container d-flex justify-content-center align-items-center vh-90">
-          <div className="card">
-            <div className="card-body p-5">
-              <form onSubmit={handleSignUp}>
-                {step === 1 && (
-                  <div>
-                    <div className="text-center mb-4">
-                      <h1 className="h3 mb-0">Registro</h1>
-                      <p className="lead">Selecciona si eres profesor o estudiante</p>
-                    </div>
-                    <div className="d-flex justify-content-center mb-4">
-                      <button type="button" className="btn btn-primary mr-3" onClick={handleTeacherClick}>Profesor</button>
-                      <button type="button" className="btn btn-primary" onClick={handleStudentClick}>Estudiante</button>
-                    </div>
-                    <div className="text-center mt-4">
-                          <span className="fs-5">¿Ya tienes una cuenta?</span>
-                      </div>
-                      <button type="button" className="btn btn-outline-secondary mx-auto mt-3 w-100" onClick={() => window.location.replace('/signin')}>
-                          <span className="fs-5">Inicia sesión</span>
-                      </button>
-                  </div>
-                )}
-                {step === 2 && (
-                  <div>
-                    <div className="text-center mb-4">
-                      <h1 className="h3 mb-0">Registro</h1>
-                      <p className="lead">Ingresa tu correo electrónico</p>
-                    </div>
-                    <div className="form-group mb-4">
-                      <label htmlFor="email" className="text-center fs-4">Correo electrónico</label>
-                      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Correo electrónico" required />
-                      {!isEmailValid() && (
-                        <div className="text-danger">El correo electrónico es inválido</div>
-                      )}
-                    </div>
-                    <div className="d-flex justify-content-center mb-4">
-                      <button type="button" className="btn btn-secondary mr-3" onClick={handlePrevStep}>Atrás</button>
-                      <button type="button" className="btn btn-primary" onClick={handleNextStep} disabled={!isEmailValid()}>Siguiente</button>
-                    </div>
-                  </div>
-                )}
-                {step === 3 && (
-                  <div>
-                    <div className="text-center mb-4">
-                      <h1 className="h3 mb-0">Registro</h1>
-                      <p className="lead">Completa tu información personal</p>
-                    </div>
-                    <div className="form-group mb-4">
-                      <label htmlFor="name" className="text-center fs-4">Nombre(s)</label>
-                      <input 
-                        type="text" 
-                        id="name" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value)} 
-                        className="form-control" 
-                        placeholder="Nombre(s)" 
-                        required 
-                      />
-                    </div>
-                    <div className="form-group mb-4">
-                      <label htmlFor="lastName1" className="text-center fs-4">Apellido paterno</label>
-                      <input 
-                        type="text" 
-                        id="lastName1" 
-                        value={lastName1} 
-                        onChange={(e) => setLastName1(e.target.value)} 
-                        className="form-control" 
-                        placeholder="Apellido paterno" 
-                        required 
-                      />
-                    </div>
-                    <div className="form-group mb-4">
-                      <label htmlFor="lastName2" className="text-center fs-4">Apellido materno</label>
-                      <input 
-                        type="text" 
-                        id="lastName2" 
-                        value={lastName2} 
-                        onChange={(e) => setLastName2(e.target.value)} 
-                        className="form-control" 
-                        placeholder="Apellido materno" 
-                        required 
-                      />
-                    </div>
-                    <button 
-                      type="button" 
-                      className="btn btn-secondary mr-3" 
-                      onClick={handlePrevStep}
-                    >
-                      Atrás
-                    </button>
-                    <button 
-                      type="button" 
-                      className="btn btn-primary" 
-                      onClick={handleNextStep}
-                    >
-                      Siguiente
-                    </button>
-                  </div>
-                )}
-                {step === 4 && (
-                  <div>
-                    <div className="text-center mb-4">
-                      <h1 className="h3 mb-0">Registro</h1>
-                      <p className="lead">Crea una contraseña</p>
-                    </div>
-                  <div className="form-group mb-4">
-                      <label htmlFor="password" className="text-center fs-4">Contraseña</label>
-                      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Contraseña" required />
-                  </div>
-                  <div className="form-group mb-4">
-                      <label htmlFor="confirmPassword" className="text-center fs-4">Confirma tu contraseña</label>
-                      <input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="form-control" placeholder="Confirmar contraseña" required />
-                      {!isPasswordValid() && (
-                        <div className="text-danger">Las contraseñas no coinciden</div>
-                      )}
-                  </div>
-                  <div className="form-check mb-4">
-                      <input type="checkbox" id="terms" checked={terms} onChange={(e) => setTerms(e.target.checked)} className="form-check-input" />
-                      <label htmlFor="terms" className="form-check-label">
-                      Acepto los términos y condiciones
-                      </label>
-                  </div>
-                      <button type="button" className="btn btn-secondary mr-3" onClick={handlePrevStep}>Atrás</button>
-                      <button type="button" className="btn btn-primary" onClick={handleNextStep} disabled={!isPasswordValid() || !terms}>Siguiente</button>
-                  </div>
-                )}
-                {step === 5 && (
-                  <div>
-                    <div className="text-center mb-4">
-                      <h1 className="h3 mb-0">Registro</h1>
-                      <p className="lead">Confirma tus datos</p>
-                    </div>
-                  <div className="text-left mb-4">
-                      <p className="fs-5">Nombre completo:</p>
-                      <p className="fs-6">{name} {lastName1} {lastName2}</p>
-                      <p className="fs-5">Correo electrónico:</p>
-                      <p className="fs-6">{email}</p>
-                      <p className="fs-5">Tipo de usuario:</p>
-                      <p className="fs-6">{isTeacher ? 'Profesor' : 'Estudiante'}</p>
-                  </div>
-                      <button type="button" className="btn btn-secondary mr-3" onClick={handlePrevStep}>Atrás</button>
-                      <button type="submit" className="btn btn-primary">Registrarse</button>
-                  </div>
-                )}
-              </form>
+      <section id="signUpForm" className='container-cc'>
+        <form onSubmit={handleSignUp}>
+
+          {step === 1 && (
+            <div>
+              <div className="text-center mb-4">
+                <h3 className="mb-0">Registro</h3>
+                <span>Selecciona si eres profesor o estudiante</span>
+              </div>
+
+              <div className="select">
+                <CustomButton
+                  type={'btn btnPrimary btnResize'}
+                  text={'Profesor'}
+                  func={handleTeacherClick}/>
+                <CustomButton
+                  type={'btn btnPrimary btnResize'}
+                  text={'Estudiante'}
+                  func={handleStudentClick}/>
+              </div>
+
+              <div className="text-center mt-4">
+                    <span>¿Ya tienes una cuenta?</span>
+                    <CustomButton
+                      type={'btn mt-3 btnSecondary'}
+                      text={'Inicia sesión'}
+                      func={() => window.location.replace('/signin')}/>
+                </div>
             </div>
-          </div>
-        </div>
+          )}
+
+          {step === 2 && (
+            <div>
+
+              <div className="text-center mb-5">
+                <h3 className="mb-0">Registro</h3>
+                <span>Ingresa tu correo electrónico</span>
+              </div>
+
+              <div className="form-group mb-4">
+                <label htmlFor="email" className="text-center">Correo electrónico</label>
+                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="your.email@tec.mx" required />
+                {!isEmailValid() && (
+                  <div className="text-danger">El correo electrónico es inválido</div>
+                )}
+              </div>
+
+              <div className="select next-back mt-5">
+                <CustomButton
+                  type={'btn btnSecondary'}
+                  text={'Atrás'}
+                  func={handlePrevStep}/>
+                <CustomButton
+                  type={'btn  btnPrimary btn-primary'}
+                  text={'Siguiente'}
+                  func={handleNextStep}
+                  disabled={!isEmailValid()}/>
+              </div>
+
+            </div>
+          )}
+
+          {step === 3 && (
+            <div>
+              <div className="text-center mb-4">
+                <h1 className="h3 mb-0">Registro</h1>
+                <p className="lead">Completa tu información personal</p>
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="name" className="text-center fs-4">Nombre(s)</label>
+                <input 
+                  type="text" 
+                  id="name" 
+                  value={name} 
+                  onChange={(e) => setName(e.target.value)} 
+                  className="form-control" 
+                  placeholder="Nombre(s)" 
+                  required 
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="lastName1" className="text-center fs-4">Apellido paterno</label>
+                <input 
+                  type="text" 
+                  id="lastName1" 
+                  value={lastName1} 
+                  onChange={(e) => setLastName1(e.target.value)} 
+                  className="form-control" 
+                  placeholder="Apellido paterno" 
+                  required 
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="lastName2" className="text-center fs-4">Apellido materno</label>
+                <input 
+                  type="text" 
+                  id="lastName2" 
+                  value={lastName2} 
+                  onChange={(e) => setLastName2(e.target.value)} 
+                  className="form-control" 
+                  placeholder="Apellido materno" 
+                  required 
+                />
+              </div>
+              <button 
+                type="button" 
+                className="btn btn-secondary mr-3" 
+                onClick={handlePrevStep}
+              >
+                Atrás
+              </button>
+              <button 
+                type="button" 
+                className="btn btn-primary" 
+                onClick={handleNextStep}
+              >
+                Siguiente
+              </button>
+            </div>
+          )}
+
+          {step === 4 && (
+            <div>
+              <div className="text-center mb-4">
+                <h1 className="h3 mb-0">Registro</h1>
+                <p className="lead">Crea una contraseña</p>
+              </div>
+            <div className="form-group mb-4">
+                <label htmlFor="password" className="text-center fs-4">Contraseña</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Contraseña" required />
+            </div>
+            <div className="form-group mb-4">
+                <label htmlFor="confirmPassword" className="text-center fs-4">Confirma tu contraseña</label>
+                <input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="form-control" placeholder="Confirmar contraseña" required />
+                {!isPasswordValid() && (
+                  <div className="text-danger">Las contraseñas no coinciden</div>
+                )}
+            </div>
+            <div className="form-check mb-4">
+                <input type="checkbox" id="terms" checked={terms} onChange={(e) => setTerms(e.target.checked)} className="form-check-input" />
+                <label htmlFor="terms" className="form-check-label">
+                Acepto los términos y condiciones
+                </label>
+            </div>
+                <button type="button" className="btn btn-secondary mr-3" onClick={handlePrevStep}>Atrás</button>
+                <button type="button" className="btn btn-primary" onClick={handleNextStep} disabled={!isPasswordValid() || !terms}>Siguiente</button>
+            </div>
+          )}
+
+          {step === 5 && (
+            <div>
+              <div className="text-center mb-4">
+                <h1 className="h3 mb-0">Registro</h1>
+                <p className="lead">Confirma tus datos</p>
+              </div>
+            <div className="text-left mb-4">
+                <p className="fs-5">Nombre completo:</p>
+                <p className="fs-6">{name} {lastName1} {lastName2}</p>
+                <p className="fs-5">Correo electrónico:</p>
+                <p className="fs-6">{email}</p>
+                <p className="fs-5">Tipo de usuario:</p>
+                <p className="fs-6">{isTeacher ? 'Profesor' : 'Estudiante'}</p>
+            </div>
+                <button type="button" className="btn btn-secondary mr-3" onClick={handlePrevStep}>Atrás</button>
+                <button type="submit" className="btn btn-primary">Registrarse</button>
+            </div>
+          )}
+
+        </form>
       </section>
     </div>
   );
