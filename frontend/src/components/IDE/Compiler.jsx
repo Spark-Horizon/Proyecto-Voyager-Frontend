99e448cb49935124048613949984a2634cd79747
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-04-20 14:12:32 -0600

    Changed some stuff in the compiler

--- frontend/src/components/IDE/Compiler.jsx
+++ frontend/src/components/IDE/Compiler.jsx
@@ -8,13 +8,14 @@ import { useSubmit } from '../../hooks/useSubmit';
 
 
 export const Compiler = () => {
-    const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+    const [code, setCode] = useState(`def main():\n    return 0`);
     const { stdOut, error, fetchSubmissionData } = useSubmit(code);
     
     return (
         <>
             <div className='compiler-container'>
                 <div className="compiler-buttons-container">
+                    <CustomButton text={"Run"} func={ fetchSubmissionData } customClass={"run"} />
                     <CustomButton text={"Submit"} func={ fetchSubmissionData } customClass={"submit"} />
                 </div>
                 <CodeEditor
@@ -23,12 +24,10 @@ export const Compiler = () => {
                     placeholder="Please enter Python code."
                     onChange={(evn) => setCode(evn.target.value)}
                     data-color-mode="dark"
-                    padding={15}
-                    className="compiler"
+                    padding={10}
                     style={{
-                        overflow: 'scroll',
                         fontSize: 12,
-                        borderRadius: '20px',
+                        borderRadius: '0px 0px 10px 10px',
                         fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
                     }}
                 />
