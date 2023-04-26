68406a65da326e359536d45534631d99a474911f
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-04-26 00:34:57 -0600

    [COMPILER] Added logic to IDE

--- frontend/src/routes/IdePage.jsx
+++ frontend/src/routes/IdePage.jsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom';
 import { Compiler } from "../components/IDE/Compiler"
 import { CustomNavbar } from "../components/CustomNavbar"
+import { Compiler } from "../components/IDE/Compiler"
+import { OutputPanel } from "../components/IDE/OutputPanel";
+import { useRunSubmit } from "../hooks/useRunSubmit";
 
 import '../styles/idePage.css';
 
 
 export const IdePage = () => {
+  const [driver, setDriver] = useState('main_test'); 
+  const [tests, setTests] = useState([{
+    input: '1, 2',
+    output: '3'
+  }]); 
+  const [code, setCode] = useState('');
+  const [id, setId] = useState(0);
+
+  const { compInfo, stdOut, stdErr, axiosError, setSubmitData, fetchSubmissionData } = useRunSubmit();
 
   // Links y componentes de Navbar
   const links = [
@@ -21,7 +33,23 @@ export const IdePage = () => {
     <div>
       <CustomNavbar links={links} components={components}/>
       <div className="ide-main-container">
-        <Compiler />
+        <Compiler 
+          tests={tests} 
+          driver={driver}
+          setCode={setCode} 
+          code={code} 
+          setSubmitData={setSubmitData}
+          fetchSubmissionData={fetchSubmissionData}
+        />
+        <OutputPanel 
+          code={code} 
+          tests={tests} 
+          driver={driver} 
+          id={id} 
+          compInfo={compInfo} 
+          stdOut={stdOut}
+          stdErr={stdErr}
+        />
       </div>
     </div>
   )
