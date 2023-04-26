8612794fce13af21873a01d151e7d2a0df62e379
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-04-26 11:29:39 -0600

    Added styles to console

--- frontend/src/components/IDE/Console.jsx
+++ frontend/src/components/IDE/Console.jsx
@@ -3,10 +3,6 @@ export const Console = ({stdOut}) => {
     return (
         <div className="compiler-console">
             <p
-                style={{
-                    font: 'Consolas',
-                    color: '#fff'
-                }}
             >{stdOut}</p>
         </div>
     )
