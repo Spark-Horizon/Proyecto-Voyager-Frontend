a11eee9870f34e281ede998842982924c924e274
Alastor963 <alaned01@gmail.com>
2023-04-27 13:54:23 -0600

    [ROUTE] CRUD update with nested dropdown for filtering

--- frontend/src/routes/CRUD.jsx
+++ frontend/src/routes/CRUD.jsx
@@ -3,7 +3,6 @@ import { CustomNavbar } from '../components/CustomNavbar';
 import { CustomButton } from '../components/CustomButton';
 
 import '../styles/forms.css'
-import '../styles/buttons.css'
 
 export const CRUD = () => {
 
@@ -21,10 +20,8 @@ export const CRUD = () => {
   const links = [];
   const components = [];
 
-  // Opciones de filtro
-
   // Opciones de cada columna
-  const columnOptions = ['', '', '', '', '', '', ''];
+  const columnOptions = ['', '', '', '', '', ''];
 
   return (
     <div>
@@ -42,9 +39,18 @@ export const CRUD = () => {
                   <select
                     className="form-select form-select-sm"
                     aria-label="Filtro"
+                    value={columnOptions[0]}
+                    onChange={(e) => columnOptions[0] = e.target.value}>
+                    <option value="">Autor</option>
+                  </select>
+                </th>
+                <th scope="col">
+                  <select
+                    className="form-select form-select-sm"
+                    aria-label="Filtro"
                     value={columnOptions[1]}
                     onChange={(e) => columnOptions[1] = e.target.value}>
-                    <option value="">Autor</option>
+                    <option value="">Subtema</option>
                   </select>
                 </th>
                 <th scope="col">
@@ -53,7 +59,7 @@ export const CRUD = () => {
                     aria-label="Filtro"
                     value={columnOptions[2]}
                     onChange={(e) => columnOptions[2] = e.target.value}>
-                    <option value="">Subtema</option>
+                    <option value="">Tipo</option>
                   </select>
                 </th>
                 <th scope="col">
@@ -62,7 +68,7 @@ export const CRUD = () => {
                     aria-label="Filtro"
                     value={columnOptions[3]}
                     onChange={(e) => columnOptions[3] = e.target.value}>
-                    <option value="">Tipo</option>
+                    <option value="">Dificultad</option>
                   </select>
                 </th>
                 <th scope="col">
@@ -71,15 +77,6 @@ export const CRUD = () => {
                     aria-label="Filtro"
                     value={columnOptions[4]}
                     onChange={(e) => columnOptions[4] = e.target.value}>
-                    <option value="">Dificultad</option>
-                  </select>
-                </th>
-                <th scope="col">
-                  <select
-                    className="form-select form-select-sm"
-                    aria-label="Filtro"
-                    value={columnOptions[5]}
-                    onChange={(e) => columnOptions[5] = e.target.value}>
                     <option value="">Aprobado</option>
                   </select>
                 </th>
@@ -90,13 +87,13 @@ export const CRUD = () => {
             <tbody>
               {rows.filter((row) => true).map((row) => (
                 <tr key={row.id}>
+                    <td>{row.col0}</td>
                     <td>{row.col1}</td>
                     <td>{row.col2}</td>
                     <td>{row.col3}</td>
                     <td>{row.col4}</td>
                     <td>{row.col5}</td>
                     <td>{row.col6}</td>
-                    <td>{row.col7}</td>
                     <td>
                     <CustomButton
                         type={'btn btn-primary btn-sm mr-2'}
@@ -112,6 +109,33 @@ export const CRUD = () => {
             <CustomButton
                 type={'btn btn-primary btn-block'}
                 text={'Añadir fila'}/>
+        </div>
+        <div>
+        <select
+          className="form-select form-select-sm"
+          aria-label="Filtro"
+          value={columnOptions[5]}
+          onChange={(e) => columnOptions[5] = e.target.value}>
+          <option value="">Filtros</option>
+          <optgroup label="Ascendente">
+            <option value="Option 1">ID</option>
+            <option value="Option 2">Título</option>
+            <option value="Option 3">Autor</option>
+            <option value="Option 4">Subtema</option>
+            <option value="Option 5">Tipo</option>
+            <option value="Option 6">Dificultad</option>
+            <option value="Option 7">Aprobado</option>
+          </optgroup>
+          <optgroup label="Descendente">
+            <option value="Option 8">ID</option>
+            <option value="Option 9">Título</option>
+            <option value="Option 10">Autor</option>
+            <option value="Option 11">Subtema</option>
+            <option value="Option 12">Tipo</option>
+            <option value="Option 13">Dificultad</option>
+            <option value="Option 14">Aprobado</option>
+          </optgroup>
+        </select>
         </div>
       </section>
 
