60ef6dfde4d9ccb6c525625944fdf1278babeeb1
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-04-28 13:21:09 -0600

    itera sobre los tests y dice si paso o no

--- frontend/src/components/IDE/TestCases.jsx
+++ frontend/src/components/IDE/TestCases.jsx
@@ -1,8 +1,23 @@
+export const TestCases = ({ tests }) => {
+  return (
+    <div className="test-cases">
+      <h1>Testcases</h1>
+      {tests && tests.map((test, index) => {
+        const passed = test.passed ? 'Passed' : 'Failed';
+        const statusClass = test.passed ? 'test-passed' : 'test-failed';
 
-export const TestCases = () => {
-    return (
-        <div className="test-cases">
-            <h1>Testcases</h1>
-        </div>
-    )
-}
+        return (
+          <div key={`testcase-${index}`} className={`test-case ${statusClass}`}>
+            <h2>Testcase {index + 1}: {passed}</h2>
+            <div className="test-case-details">
+              <p><strong>Input:</strong> {test.input}</p>
+              <p><strong>Expected Output:</strong> {test.expectedOutput}</p>
+              <p><strong>Actual Output:</strong> {test.actualOutput}</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
