c0e761bc84af5acf0f2a27d2a94fe9021b5232d2
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-24 01:15:11 -0600

    Ya jala xd

--- frontend/src/routes/Groups.jsx
+++ frontend/src/routes/Groups.jsx
@@ -1,7 +1,53 @@
-export const Groups = ({user}) => {
+import { useEffect, useState } from "react";
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { getGroups, deleteGroup } from "../helpers/Groups/api";
+import { NewGroupModal } from "../components/Groups/NewGroupModal";
+
+export const Groups = ({ user }) => {
+  // Create state variables
+  const [groups, setGroups] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  // Use Effect to get the groups on first render
+  useEffect(() => {
+    fetchGroups();
+  }, []);
+
+  // Fetch groups from API
+  const fetchGroups = async () => {
+    const fetchedGroups = await getGroups(user.id);
+    console.log(fetchedGroups);
+    setGroups(fetchedGroups);
+  };
+
+  // Delete a group
+  const handleDelete = async (id) => {
+    await deleteGroup(id);
+    fetchGroups();
+  };
+
+  // Return the JSX for the component
   return (
-    <section>
-        <h2>{user.id}</h2>
-    </section>
-  )
-}
+    <Container>
+      {/* Button to open the NewGroupModal */}
+      <Button onClick={() => setShowModal(true)} className="mb-3">Create Group</Button>
+
+      {/* Modal for creating new group */}
+      <NewGroupModal user={user} show={showModal} onHide={() => setShowModal(false)} onGroupCreated={fetchGroups} />
+
+      {/* Display groups */}
+      <Row>
+        {groups.map((group, index) => (
+          <Col md={4} key={index}>
+            <Card>
+              <Card.Body>
+                <Card.Title>{group.idMateriaGrupo}</Card.Title>
+                <Button variant="danger" onClick={() => handleDelete(group.id)}>Delete</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
