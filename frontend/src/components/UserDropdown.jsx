a5e2a1fe1ff675570218aacfdaf1ff4e41e57453
ale_wsl <alejo.ubeto@gmail.com>
2023-05-27 21:31:04 -0600

    [COMPONENT] user dropdown done

--- frontend/src/components/UserDropdown.jsx
+++ frontend/src/components/UserDropdown.jsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CustomButton } from './CustomButton';
+import { useAuth } from '../hooks/AuthContext';
 
 export const UserDropdown = (props) => {
+  
+  // Estados del componente
   const [isOpen, setIsOpen] = useState(false);
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+  const { logout } = useAuth()
+
+  // Funcionalidades del componente
+  async function handleLogout(){
+    setError('')
+    try {
+      await logout()
+      navigate('/')
+
+    } catch {}
+  }
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
-    console.log(isOpen)
   };
 
   return (
     <div className="dropdown">
-      <button className="dropdown-toggle" onClick={toggleDropdown}>
-        Dropdown Button
+      <button className="userDropdown-btn dropdown-toggle" onClick={toggleDropdown} data-bs-toggle="dropdown">
+        {props.user.name}
       </button>
       {isOpen && (
-        <ul className="dropdown-menu">
-          <li><a href="/">Option 1</a></li>
-          <li><a href="/">Option 2</a></li>
-          <li><a href="/">Option 3</a></li>
+        <ul className="userDropdown-menu dropdown-menu dropdown-menu-right dropdown-menu-dark">
+          
+          <div className='userInfo'>
+            <h6 className='dropdown-header fw-bold'>Hola, {props.user.name}</h6>
+            <p className='dropdown-header'>{props.user.id}</p>
+          </div>
+
+          <li><hr className="dropdown-divider"/></li>
+          <li><CustomButton type={'dropdown-item btn-sm'} text={'Cerrar sesión'} func={handleLogout}/></li>
         </ul>
       )}
     </div>
