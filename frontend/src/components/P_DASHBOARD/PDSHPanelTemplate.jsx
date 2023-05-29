51bbece6facf2f9f7f40e2e06693102ac8465a51
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-05-29 16:51:44 -0600

    [PDASHBOARD] Función de barra de búsqueda removida

--- frontend/src/components/P_DASHBOARD/PDSHPanelTemplate.jsx
+++ frontend/src/components/P_DASHBOARD/PDSHPanelTemplate.jsx
@@ -4,11 +4,12 @@ export const PDSHPanelTemplate = ({ title, queryFunction }) => {
     return (
         <div className='p-dash-top-container'>
             <h1>{title}</h1>
+            {/* SEARCH BAR IMPLEMENTATION
             <form className="p-dash-top-container-search-bar">
                 <input type="text" name="" id="" className='search-bar-input' />
                 <span className='search-bar-placeholder'>Search</span>
                 <MagnifyingGlassIcon className='magnifying-glass'/>
-            </form>
+            </form> */}
         </div>
     )
 }
