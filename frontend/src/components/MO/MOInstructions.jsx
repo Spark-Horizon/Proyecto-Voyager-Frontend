dd375fcfa45d16ae694ce9bfef1ac5f37832e974
ale_wsl <alejo.ubeto@gmail.com>
2023-05-29 22:46:59 -0600

    [MISC] minor changes

--- frontend/src/components/MO/MOInstructions.jsx
+++ frontend/src/components/MO/MOInstructions.jsx
@@ -1,3 +1,4 @@
+import { CustomButton } from '../CustomButton'
 import { useEffect, useState } from "react";
 import { useGetTask } from "../../hooks/useGetTask";
 import { useGetProgress } from "../../hooks/useGetProgress";
