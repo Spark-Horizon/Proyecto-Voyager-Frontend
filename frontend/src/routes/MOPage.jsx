d189aeec754f2d3baf58bbb41977870ea8aea94d
ale_wsl <alejo.ubeto@gmail.com>
2023-05-29 23:15:55 -0600

    [MISC] minor changes

--- frontend/src/routes/MOPage.jsx
+++ frontend/src/routes/MOPage.jsx
@@ -1,10 +1,19 @@
+import { CustomButton } from '../components/CustomButton'
 import { MOInstructions } from "../components/MO/MOInstructions"
 import '../styles/moPage.css'
 
 export const MOPage = () => {
+
+    const handleSkip = (e) => {
+        e.preventDefault();
+        // on progress
+        return 0
+    };
+
     return(
         <div className="mo-route-container">
             <div className="mopage-main-container container-cc">
+                <CustomButton type={'btn btnPrimary btn-sm'} func={handleSkip} text={'Saltar'}/>
                 <MOInstructions/>
             </div>
         </div>
