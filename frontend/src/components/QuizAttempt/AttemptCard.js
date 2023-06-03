4306f27b7197142362d74934c21267dd5d3a5044
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-06-02 23:34:14 -0600

    Individual Answers Shown

--- frontend/src/components/QuizAttempt/AttemptCard.js
+++ frontend/src/components/QuizAttempt/AttemptCard.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 export const AttemptCard = ({ eventKey, attempt }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Card className="attempt-card">
       <Card.Header>
-        <Button variant="link">
+        <Button variant="link" onClick={handleToggle}>
           Intento {eventKey + 1}
         </Button>
       </Card.Header>
-      <Card.Body>
-        <Card.Text>Inicio: {attempt.inicio}</Card.Text>
-        <Card.Text>Fin: {attempt.fin}</Card.Text>
-        <Card.Text>Respuestas Correctas: {attempt.correctos}</Card.Text>
-        <Card.Text>Respuestas Incorrectas: {attempt.incorrectos}</Card.Text>
-        {/* Aquí puedes iterar sobre las respuestas y mostrar los detalles */}
-      </Card.Body>
+      {isOpen && (
+        <Card.Body>
+          <Card.Text>Inicio: {attempt.inicio}</Card.Text>
+          <Card.Text>Fin: {attempt.fin}</Card.Text>
+          <Card.Text>Respuestas Correctas: {attempt.correctos}</Card.Text>
+          <Card.Text>Respuestas Incorrectas: {attempt.incorrectos}</Card.Text>
+          {attempt.answers.respuestas.map((respuesta, index) => (
+            <Card.Text key={index}>
+              Respuesta {index+1}: {respuesta.respuesta ? respuesta.respuesta : 'N/A'} - Correcto: {respuesta.correcto ? 'Si' : 'No'}
+            </Card.Text>
+          ))}
+        </Card.Body>
+      )}
     </Card>
   );
 };
-
