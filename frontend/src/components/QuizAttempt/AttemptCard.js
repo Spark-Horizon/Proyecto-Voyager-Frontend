471809d25899ec51e5a0029d246db927512c92b3
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-06-02 23:16:07 -0600

    Ya jala

--- frontend/src/components/QuizAttempt/AttemptCard.js
+++ frontend/src/components/QuizAttempt/AttemptCard.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Card, Accordion, Button } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 export const AttemptCard = ({ eventKey, attempt }) => {
   return (
-    <Accordion>
-      <Card className="attempt-card">
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey={eventKey}>
-            Intento {eventKey + 1}
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey={eventKey}>
-          <Card.Body>
-            <Card.Text>Inicio: {attempt.inicio}</Card.Text>
-            <Card.Text>Fin: {attempt.fin}</Card.Text>
-            <Card.Text>Respuestas Correctas: {attempt.correctos}</Card.Text>
-            <Card.Text>Respuestas Incorrectas: {attempt.incorrectos}</Card.Text>
-            {/* Aquí puedes iterar sobre las respuestas y mostrar los detalles */}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <Card className="attempt-card">
+      <Card.Header>
+        <Button variant="link">
+          Intento {eventKey + 1}
+        </Button>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>Inicio: {attempt.inicio}</Card.Text>
+        <Card.Text>Fin: {attempt.fin}</Card.Text>
+        <Card.Text>Respuestas Correctas: {attempt.correctos}</Card.Text>
+        <Card.Text>Respuestas Incorrectas: {attempt.incorrectos}</Card.Text>
+        {/* Aquí puedes iterar sobre las respuestas y mostrar los detalles */}
+      </Card.Body>
+    </Card>
   );
 };
+
