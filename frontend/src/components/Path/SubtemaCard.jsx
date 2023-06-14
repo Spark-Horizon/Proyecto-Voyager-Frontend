48ee1ffcf09af4dc6eb797bbfd6602641988a2fd
ale_wsl <alejo.ubeto@gmail.com>
2023-06-13 22:14:00 -0600

    [STYLES] strikes done

--- frontend/src/components/Path/SubtemaCard.jsx
+++ frontend/src/components/Path/SubtemaCard.jsx
@@ -22,22 +22,47 @@ export const SubtemaCard = (props) => {
         }
     }
 
-    // Mostrar racha (user/tema) y requeridos (user/tema)
-    const goals = (id_subtem, type) => {
-        let racha = (typeInfo[id_subtem][type].uRacha / typeInfo[id_subtem][type].racha) * 100
-        let requeridos = (typeInfo[id_subtem][type].uRequeridos / typeInfo[id_subtem][type].requeridos) * 100
+    // Mostrar requeridos (user/tema)
+    const requeridos = (id_subtem, type) => {
+        let reqs = (typeInfo[id_subtem][type].uRequeridos / typeInfo[id_subtem][type].requeridos) * 100
         return (
-            <div className="goals">
-                <div className="subtema-racha">
-                    <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: '5px' }}>
-                        <div class="progress-bar" style={{ width: racha + '%' }}/>
-                    </div>
+            <div className="subtema-requeridos">
+                <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: '10px' }}>
+                    <div class="mt-1 customProgress progress-bar progress-bar-striped progress-bar-animated" style={{ width: reqs + '%' }}/>
                 </div>
-                <div className="subtema-requeridos">
-                    <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: '10px' }}>
-                        <div class="customProgress progress-bar progress-bar-striped progress-bar-animated" style={{ width: requeridos + '%' }}/>
-                    </div>
-                </div>
+            </div>
+        )
+    }
+
+    // Rachas
+    const generarStrikes = (n) => {
+        const strikes = [];
+        if(n === 0)
+            strikes.push(<p>-</p>);
+
+        for (let i = 0; i < n; i++) {
+        strikes.push(<p key={i}>🔥</p>);
+        }
+    
+        return strikes;
+    };
+    // Mostrar racha (user/tema)
+    const racha = (id_subtem, type) => {
+        let strike = (typeInfo[id_subtem][type].uRacha / typeInfo[id_subtem][type].racha) * 100
+        if (strike >= 80) {
+            strike = 4;
+        } else if (strike >= 60) {
+            strike = 3;
+        } else if (strike >= 40) {
+            strike = 2;
+        } else if (strike >= 20) {
+            strike = 1;
+        } else {
+            strike = 0;
+        }
+        return (
+            <div className="excercise-racha">
+                {generarStrikes(strike)}
             </div>
         )
     }
@@ -74,22 +99,24 @@ export const SubtemaCard = (props) => {
                 ) : null}
                 <div className="select">
                     <div className="btnResize excercise-link">
-                        {goals(props.id, "mo")}
+                        {racha(props.id, "mo")}
                         <Link
                             to={{ pathname: '/MOPage' }}
                             state={{ subtem: props.id, practice_mode: practice(props.id), path:(props.path), materia: (props.materia)}}
                             style={available(props.id, "mo")}>
                             <CustomButton text={"Opción Múltiple"} type={"btn btn-sm btnPrimary selectBtn"}/>
                         </Link>
+                        {requeridos(props.id, "mo")}
                     </div>
                     <div className="btnResize excercise-link">
-                        {goals(props.id, "c")}
+                        {racha(props.id, "c")}
                         <Link
                             to={{ pathname: '/Compiler' }}
                             state={{ subtem: props.id, practice_mode: practice(props.id), path:(props.path), materia: (props.materia)}}
                             style={available(props.id, "c")}>
                             <CustomButton text={"Código"} type={"btn btn-sm btnPrimary selectBtn"}/>
                         </Link>
+                        {requeridos(props.id, "c")}
                     </div>
                 </div>
             </div>
