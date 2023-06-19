8cdd6522c638fb3cf29e37181b52231fca05f2a0
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-06-18 21:51:14 -0600

    Agregué botón de finalizar

--- frontend/src/components/QuizStudent/QuizPage.jsx
+++ frontend/src/components/QuizStudent/QuizPage.jsx
@@ -5,26 +5,31 @@ import { useAuth } from '../../hooks/AuthContext';
 import { useGetOrSet } from '../../hooks/QuizStudent/useGetOrSet';
 import { useParams } from 'react-router-dom';
 import { useSubmitRespuesta } from '../../hooks/QuizStudent/useSubmitRespuesta';
+import { useSubmitIntento } from '../../hooks/QuizStudent/useSubmitIntento';
 
 export const QuizPage = () => {
   const { user } = useAuth();
   const matricula = user.id;
-  const { id_activity:quiz } = useParams()
+  const { id_activity: quiz } = useParams()
 
   const { loading, error, data } = useGetOrSet(matricula, quiz);
-  const { loading:submitLoading, error:submitError, submitRespuesta } = useSubmitRespuesta();
+  const { loading: submitLoading, error: submitError, submitRespuesta } = useSubmitRespuesta();
+  const { loading: finishLoading, error: finishError, submitIntento } = useSubmitIntento();
 
-  const [renderizedExercises, setRenderizedExercises] = useState(length(data.respuestas.length));
-  const [id_respuesta, setIdRespuesta] = useState(null);
+  const [renderizedExercises, setRenderizedExercises] = useState(0);
+  const [id_intento, setIdIntento] = useState(null);
 
-  const handleSubmitRespuesta = (answer_JSON) => {
-    useSubmitRespuesta(id_respuesta ,answer_JSON)
-    if (!submitError && !submitLoading){
+
+  const handleSubmitRespuesta = (id_respuesta, answer_JSON) => {
+    submitRespuesta(id_respuesta, answer_JSON)
+    if (!submitError && !submitLoading) {
       setRenderizedExercises(renderizedExercises - 1);
     }
   }
-  
 
+  const handleSubmitIntento = () => {
+    submitIntento(id_intento)
+  }
 
   useEffect(() => {
     if (!loading) {
@@ -32,23 +37,48 @@ export const QuizPage = () => {
         console.error(error);
       } else {
         console.log(data);
+        setRenderizedExercises(data.respuestas.length);
+        setIdIntento(data.intento)
       }
     }
   }, [loading, error, data]);
 
   return (
     <div>
-      {data ? 
+      {data ?
         <div>
-          {data.respuestas.map((respuesta) => 
-            respuesta.tipo === "Código" 
-            ? <CompilerPage data={respuesta.ejercicio_archivo} submitFunc={handleSubmitRespuesta} />
-            : respuesta.tipo === "Opción múltiple" 
-            ? <MultipleOptionPage data={respuesta.ejercicio_archivo} submitFunc={handleSubmitRespuesta} /> 
-            : null
+          {data.respuestas.map((respuesta) =>
+            respuesta.tipo === "Código"
+              ? <CompilerPage data={respuesta.ejercicio_archivo} submitFunc={handleSubmitRespuesta} />
+              : respuesta.tipo === "Opción múltiple"
+                ? <MultipleOptionPage data={respuesta.ejercicio_archivo} submitFunc={handleSubmitRespuesta} />
+                : null
           )}
-        </div> 
-        : 
+          <button
+            onClick={handleSubmitIntento}
+            disabled={renderizedExercises !== 0}
+            style={{
+              display: 'block',
+              width: '200px',
+              height: '50px',
+              margin: '20px auto',
+              fontSize: '18px',
+              backgroundColor: 'rgba(255, 255, 255, 0.1)',
+              border: '1px solid rgba(255, 255, 255, 0.3)',
+              backdropFilter: 'blur(10px)',
+              webkitBackdropFilter: 'blur(10px)',
+              borderRadius: '5px',
+              cursor: renderizedExercises === 0 ? 'pointer' : 'not-allowed',
+              pointerEvents: 'auto',
+              color: '#fff',
+              textAlign: 'center',
+              lineHeight: '50px'
+            }}
+          >
+            Finalizar Quiz
+          </button>
+        </div>
+        :
         <p>Loading...</p>
       }
     </div>
